9745968091d73b8a22f919c3c8c862a297a7feec
snigdho-dutta <aexseusidparker64@gmail.com>
2025-02-19 17:45:30 +0530

    Port Binding Updated

--- server/main.py
+++ server/main.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     num_processes = multiprocessing.cpu_count() - 1
     uvicorn.run(
         app="app:sio_manager.socket_app",
-        host="127.0.0.1",
-        port=8000,
+        host="0.0.0.0",
+        port=10000,
         reload=True,
         workers=num_processes,
     )
